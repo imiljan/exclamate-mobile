1ab726e2baa402bb95654b2a2af6df000efe6b6a
Miljan <miljan.ignjatovic@theblockbox.io>
2019-08-28 14:43:04 +0200

    addPost mutation and cache update

--- src/app/home/posts/posts.page.ts
+++ src/app/home/posts/posts.page.ts
@@ -2,9 +2,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Apollo } from 'apollo-angular';
 import { ApolloQueryResult } from 'apollo-client';
+import { FetchResult } from 'apollo-link';
 import gql from 'graphql-tag';
 import { Subscription } from 'rxjs';
-import { HomePagePostsQuery } from 'src/generated/graphql';
+import { AddPostMutation, HomePagePostsQuery } from 'src/generated/graphql';
 
 import { AddPostComponent } from './add-post/add-post.component';
 
@@ -14,6 +15,39 @@ import { AddPostComponent } from './add-post/add-post.component';
   styleUrls: ['./posts.page.scss'],
 })
 export class PostsPage implements OnInit, OnDestroy {
+  readonly HOME_PAGE_POSTS_QUERY = gql`
+    query homePagePosts {
+      getPosts {
+        id
+        body
+        created
+        user {
+          id
+          username
+          firstName
+          lastName
+          email
+        }
+      }
+    }
+  `;
+  readonly CREATE_POST_MUTATION = gql`
+    mutation addPost($body: String!) {
+      createPost(body: $body) {
+        id
+        body
+        created
+        user {
+          id
+          username
+          firstName
+          lastName
+          email
+        }
+      }
+    }
+  `;
+
   posts = [{ id: '1', body: 'asd', created: Date.now(), user: null }];
   isLoading = true;
 
@@ -24,22 +58,7 @@ export class PostsPage implements OnInit, OnDestroy {
   ngOnInit() {
     this.querySubscription = this.apollo
       .watchQuery({
-        query: gql`
-          query homePagePosts {
-            getPosts {
-              id
-              body
-              created
-              user {
-                id
-                username
-                firstName
-                lastName
-                email
-              }
-            }
-          }
-        `,
+        query: this.HOME_PAGE_POSTS_QUERY,
       })
       .valueChanges.subscribe(({ data, loading }: ApolloQueryResult<HomePagePostsQuery>) => {
         this.posts = data.getPosts;
@@ -49,12 +68,42 @@ export class PostsPage implements OnInit, OnDestroy {
 
   onAddPost() {
     this.modalCtrl.create({ component: AddPostComponent }).then((modalEl) => {
-      modalEl.onDidDismiss().then((modalData) => {
-        if (!modalData.data) {
+      modalEl.onDidDismiss().then(({ data, role }) => {
+        if (!data) {
           return;
         }
-        console.log(modalData.data);
-        console.log(modalData.role);
+        console.log(data);
+        console.log(role);
+        if (role === 'add') {
+          this.apollo
+            .mutate({
+              mutation: this.CREATE_POST_MUTATION,
+              variables: data,
+              update: (proxy, { data: { createPost } }: FetchResult<AddPostMutation>) => {
+                // Read the data from our cache for this query.
+                const postsInCache = proxy.readQuery<HomePagePostsQuery>({
+                  query: this.HOME_PAGE_POSTS_QUERY,
+                });
+
+                // Add our todo from the mutation to the end.
+                postsInCache.getPosts.unshift(createPost);
+
+                // Write our data back to the cache.
+                proxy.writeQuery({
+                  query: this.HOME_PAGE_POSTS_QUERY,
+                  data,
+                });
+              },
+            })
+            .subscribe(
+              (res: ApolloQueryResult<AddPostMutation>) => {
+                console.log(`Created`, res);
+              },
+              (errorResp) => {
+                console.error(`Error`, errorResp);
+              }
+            );
+        }
       });
       modalEl.present();
     });
--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -1,199 +1,222 @@
-import gql from 'graphql-tag';
 import { Injectable } from '@angular/core';
 import * as Apollo from 'apollo-angular';
+import gql from 'graphql-tag';
+
 export type Maybe<T> = T | null;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string,
-  String: string,
-  Boolean: boolean,
-  Int: number,
-  Float: number,
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
   /** Date custom scalar type */
-  Date: any,
+  Date: any;
 };
-
-
-
 
 export type Mutation = {
-  __typename?: 'Mutation',
-  _?: Maybe<Scalars['Boolean']>,
-  register: RegisterResponse,
-  createPost: Post,
+  __typename?: 'Mutation';
+  _?: Maybe<Scalars['Boolean']>;
+  register: RegisterResponse;
+  createPost: Post;
 };
-
 
 export type MutationRegisterArgs = {
-  userData?: Maybe<RegisterInput>
+  userData?: Maybe<RegisterInput>;
 };
 
-
 export type MutationCreatePostArgs = {
-  body: Scalars['String']
+  body: Scalars['String'];
 };
 
 export type Post = {
-  __typename?: 'Post',
-  id: Scalars['ID'],
-  body: Scalars['String'],
-  created: Scalars['Date'],
-  user: User,
+  __typename?: 'Post';
+  id: Scalars['ID'];
+  body: Scalars['String'];
+  created: Scalars['Date'];
+  user: User;
 };
 
 export type Query = {
-  __typename?: 'Query',
-  _?: Maybe<Scalars['Boolean']>,
-  hello: Scalars['String'],
-  login: Token,
-  me: User,
-  getPost?: Maybe<Post>,
-  getPosts: Array<Maybe<Post>>,
+  __typename?: 'Query';
+  _?: Maybe<Scalars['Boolean']>;
+  hello: Scalars['String'];
+  login: Token;
+  me: User;
+  getPost?: Maybe<Post>;
+  getPosts: Array<Maybe<Post>>;
 };
-
 
 export type QueryLoginArgs = {
-  username: Scalars['String'],
-  password: Scalars['String']
+  username: Scalars['String'];
+  password: Scalars['String'];
 };
 
-
 export type QueryGetPostArgs = {
-  id: Scalars['ID']
+  id: Scalars['ID'];
 };
 
 export type RegisterInput = {
-  username: Scalars['String'],
-  password: Scalars['String'],
-  firstName: Scalars['String'],
-  lastName: Scalars['String'],
-  email: Scalars['String'],
+  username: Scalars['String'];
+  password: Scalars['String'];
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  email: Scalars['String'];
 };
 
 export type RegisterResponse = {
-  __typename?: 'RegisterResponse',
-  user?: Maybe<User>,
-  token?: Maybe<Scalars['String']>,
+  __typename?: 'RegisterResponse';
+  user?: Maybe<User>;
+  token?: Maybe<Scalars['String']>;
 };
 
 export type Token = {
-  __typename?: 'Token',
-  token: Scalars['String'],
+  __typename?: 'Token';
+  token: Scalars['String'];
 };
 
 export type User = {
-  __typename?: 'User',
-  id: Scalars['ID'],
-  username: Scalars['String'],
-  firstName: Scalars['String'],
-  lastName: Scalars['String'],
-  email: Scalars['String'],
-  posts?: Maybe<Array<Post>>,
+  __typename?: 'User';
+  id: Scalars['ID'];
+  username: Scalars['String'];
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  email: Scalars['String'];
+  posts?: Maybe<Array<Post>>;
 };
 export type LoginUserQueryVariables = {
-  username: Scalars['String'],
-  password: Scalars['String']
+  username: Scalars['String'];
+  password: Scalars['String'];
 };
 
-
-export type LoginUserQuery = (
-  { __typename?: 'Query' }
-  & { login: (
-    { __typename?: 'Token' }
-    & Pick<Token, 'token'>
-  ) }
-);
+export type LoginUserQuery = { __typename?: 'Query' } & {
+  login: { __typename?: 'Token' } & Pick<Token, 'token'>;
+};
 
 export type RegisterUserMutationVariables = {
-  input: RegisterInput
+  input: RegisterInput;
 };
 
-
-export type RegisterUserMutation = (
-  { __typename?: 'Mutation' }
-  & { register: (
-    { __typename?: 'RegisterResponse' }
-    & Pick<RegisterResponse, 'token'>
-    & { user: Maybe<(
-      { __typename?: 'User' }
-      & Pick<User, 'id' | 'username' | 'lastName' | 'firstName' | 'email'>
-    )> }
-  ) }
-);
+export type RegisterUserMutation = { __typename?: 'Mutation' } & {
+  register: { __typename?: 'RegisterResponse' } & Pick<RegisterResponse, 'token'> & {
+      user: Maybe<
+        { __typename?: 'User' } & Pick<User, 'id' | 'username' | 'lastName' | 'firstName' | 'email'>
+      >;
+    };
+};
 
 export type HomePagePostsQueryVariables = {};
 
+export type HomePagePostsQuery = { __typename?: 'Query' } & {
+  getPosts: Array<
+    Maybe<
+      { __typename?: 'Post' } & Pick<Post, 'id' | 'body' | 'created'> & {
+          user: { __typename?: 'User' } & Pick<
+            User,
+            'id' | 'username' | 'firstName' | 'lastName' | 'email'
+          >;
+        }
+    >
+  >;
+};
 
-export type HomePagePostsQuery = (
-  { __typename?: 'Query' }
-  & { getPosts: Array<Maybe<(
-    { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'body' | 'created'>
-    & { user: (
-      { __typename?: 'User' }
-      & Pick<User, 'id' | 'username' | 'firstName' | 'lastName' | 'email'>
-    ) }
-  )>> }
-);
+export type AddPostMutationVariables = {
+  body: Scalars['String'];
+};
+
+export type AddPostMutation = { __typename?: 'Mutation' } & {
+  createPost: { __typename?: 'Post' } & Pick<Post, 'id' | 'body' | 'created'> & {
+      user: { __typename?: 'User' } & Pick<
+        User,
+        'id' | 'username' | 'firstName' | 'lastName' | 'email'
+      >;
+    };
+};
 
 export const LoginUserDocument = gql`
-    query LoginUser($username: String!, $password: String!) {
-  login(username: $username, password: $password) {
-    token
+  query LoginUser($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+    }
   }
-}
-    `;
+`;
 
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class LoginUserGQL extends Apollo.Query<LoginUserQuery, LoginUserQueryVariables> {
-    document = LoginUserDocument;
-    
-  }
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginUserGQL extends Apollo.Query<LoginUserQuery, LoginUserQueryVariables> {
+  document = LoginUserDocument;
+}
 export const RegisterUserDocument = gql`
-    mutation RegisterUser($input: RegisterInput!) {
-  register(userData: $input) {
-    user {
-      id
-      username
-      lastName
-      firstName
-      email
+  mutation RegisterUser($input: RegisterInput!) {
+    register(userData: $input) {
+      user {
+        id
+        username
+        lastName
+        firstName
+        email
+      }
+      token
     }
-    token
   }
-}
-    `;
+`;
 
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class RegisterUserGQL extends Apollo.Mutation<RegisterUserMutation, RegisterUserMutationVariables> {
-    document = RegisterUserDocument;
-    
-  }
+@Injectable({
+  providedIn: 'root',
+})
+export class RegisterUserGQL extends Apollo.Mutation<
+  RegisterUserMutation,
+  RegisterUserMutationVariables
+> {
+  document = RegisterUserDocument;
+}
 export const HomePagePostsDocument = gql`
-    query homePagePosts {
-  getPosts {
-    id
-    body
-    created
-    user {
+  query homePagePosts {
+    getPosts {
       id
-      username
-      firstName
-      lastName
-      email
+      body
+      created
+      user {
+        id
+        username
+        firstName
+        lastName
+        email
+      }
     }
   }
-}
-    `;
+`;
 
-  @Injectable({
-    providedIn: 'root'
-  })
-  export class HomePagePostsGQL extends Apollo.Query<HomePagePostsQuery, HomePagePostsQueryVariables> {
-    document = HomePagePostsDocument;
-    
+@Injectable({
+  providedIn: 'root',
+})
+export class HomePagePostsGQL extends Apollo.Query<
+  HomePagePostsQuery,
+  HomePagePostsQueryVariables
+> {
+  document = HomePagePostsDocument;
+}
+export const AddPostDocument = gql`
+  mutation addPost($body: String!) {
+    createPost(body: $body) {
+      id
+      body
+      created
+      user {
+        id
+        username
+        firstName
+        lastName
+        email
+      }
+    }
   }
+`;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AddPostGQL extends Apollo.Mutation<AddPostMutation, AddPostMutationVariables> {
+  document = AddPostDocument;
+}
