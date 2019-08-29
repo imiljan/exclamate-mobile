bbc9e0ef4b53aa7e99960f9c31ebdef6af599dfa
Miljan <miljan.ignjatovic@theblockbox.io>
2019-08-29 14:21:31 +0200

    Profile page in progress

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,12 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Subscription } from 'rxjs';
+import { ProfilePageQueryQuery, User } from 'src/generated/graphql';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
-  constructor() {}
+export class ProfilePage implements OnInit, OnDestroy {
+  readonly PROFILE_QUERY = gql`
+    query ProfilePageQuery {
+      me {
+        id
+        username
+        firstName
+        lastName
+        email
+        joinedDate
+        posts {
+          id
+          body
+          created
+          likes
+        }
+      }
+    }
+  `;
 
-  ngOnInit() {}
+  user: User;
+  isLoading = true;
+
+  private querySubscription: Subscription;
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.querySubscription = this.apollo
+      .query<ProfilePageQueryQuery>({
+        query: this.PROFILE_QUERY,
+      })
+      .subscribe(({ data, loading }) => {
+        this.user = data.me;
+        this.isLoading = loading;
+        console.log(this.user.posts);
+        this.user.posts = this.user.posts.map((el) => ({ ...el, user: this.user, comments: [] }));
+      });
+  }
+
+  ngOnDestroy() {
+    if (this.querySubscription) {
+      this.querySubscription.unsubscribe();
+    }
+  }
 }
