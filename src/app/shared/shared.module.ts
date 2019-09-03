be4dc19d567f103d916342a6b1d49b70c3b8659d
Miljan <miljan.ignjatovic@theblockbox.io>
2019-09-03 12:24:06 +0200

    picture upload in progress

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 import { CommentItemComponent } from './comment-item/comment-item.component';
+import { ImagePickerComponent } from './pickers/image-picker/image-picker.component';
 import { PostItemComponent } from './post-item/post-item.component';
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  exports: [PostItemComponent, CommentItemComponent],
-  declarations: [PostItemComponent, CommentItemComponent],
+  exports: [PostItemComponent, CommentItemComponent, ImagePickerComponent],
+  declarations: [PostItemComponent, CommentItemComponent, ImagePickerComponent],
 })
 export class SharedModule {}
